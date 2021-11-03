e1c0c8d6dca814c8803d41b4c215d0a1e13d8c19
youngbin218 <50580794+youngbin218@users.noreply.github.com>
2021-11-03 22:17:58 +0900

    AND, NAND, OR 게이트 구현

---
+++ Chapter 2/2.3.3.py
@@ -0,0 +1,29 @@
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        print("0")
+    elif tmp > 0:
+        print("1")
+        
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        print("0")
+    elif tmp > 0:
+        print("1")
+        
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        print("0")
+    elif tmp > 0:
+        print("1")
