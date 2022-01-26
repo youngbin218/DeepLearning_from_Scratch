4306e6c2ed1418ea627cf85990a3155821286bd6
youngbin218 <50580794+youngbin218@users.noreply.github.com>
2022-01-26 23:48:29 +0900

    드롭아웃

---
+++ Chapter 6/6.4.3.py
@@ -0,0 +1,44 @@
+class Dropout:
+    def __init__(self, dropout_ratio=0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+        
+    def forward(self, x, train_flg=True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else:
+            return x * (1.0 - self.dropout_ratio)
+        
+    def backward(self, dout):
+        return dout * self.mask
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
+
+x_train = x_train[:300]
+t_train = t_train[:300]
+
+use_dropout = True
+dropout_ratio = 0.2
+
+network = MultiLayerNetExtend(input_size=784, hidden_size_list=[100, 100, 100, 100, 100, 100],
+                              output_size=10, use_dropout=use_dropout, dropout_ration=dropout_ratio)
+trainer = Trainer(network, x_train, t_train, x_test, t_test,
+                  epochs=301, mini_batch_size=100,
+                  optimizer='sgd', optimizer_param={'lr': 0.01}, verbose=True)
+trainer.train()
+
+train_acc_list, test_acc_list = trainer.train_acc_list, trainer.test_acc_list
+
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, marker='o', label='train', markevery=10)
+plt.plot(x, test_acc_list, marker='s', label='test', markevery=10)
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
