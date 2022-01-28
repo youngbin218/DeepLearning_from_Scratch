3b3b5215626581d5edc34ed1f18fb3d5683c934d
youngbin218 <50580794+youngbin218@users.noreply.github.com>
2022-01-29 00:24:01 +0900

    CNN 구현

--- Chapter 7/7.5.py
+++ Chapter 7/7.5.py
@@ -1,10 +1,12 @@
+from collections import OrderedDict
+
 class SimpleConvNet:
     def __init__(self, input_dim=(1, 28, 28), conv_param={'filter_num':30, 'filter_size':5, 'pad':0, 'stride':1},
                 hidden_size=100, output_size=10, weight_init_std=0.01):
         filter_num = conv_param['filter_num']
         filter_size = conv_param['filter_size']
-        pad = conv_param['pad']
-        stride = conv_param['stride']
+        filter_pad = conv_param['pad']
+        filter_stride = conv_param['stride']
         input_size = input_dim[1]
         conv_output_size = (input_size - filter_size + 2 * filter_pad) / filter_stride + 1
         pool_output_size = int(filter_num * (conv_output_size / 2) * (conv_output_size / 2))
@@ -15,7 +17,7 @@ class SimpleConvNet:
         self.params['W2'] = weight_init_std * np.random.randn(pool_output_size, hidden_size)
         self.params['b2'] = np.zeros(hidden_size)
         self.params['W3'] = weight_init_std * np.random.randn(hidden_size, output_size)
-        self.params['b1'] = np.zeros(output_size)
+        self.params['b3'] = np.zeros(output_size)
         
         self.layers = OrderedDict()
         self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'], conv_param['stride'], conv_param['pad'])
@@ -38,6 +40,20 @@ class SimpleConvNet:
         
         return self.last_layer.forward(y, t)
     
+    def accuracy(self, x, t, batch_size=100):
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
+        
+        acc = 0.0
+        
+        for i in range(int(x.shape[0] / batch_size)):
+            tx = x[i*batch_size:(i+1)*batch_size]
+            tt = t[i*batch_size:(i+1)*batch_size]
+            y = self.predict(tx)
+            y = np.argmax(y, axis=1)
+            acc += np.sum(y == tt) 
+        
+        return acc / x.shape[0]
+    
     def gradient(self, x, t):
         self.loss(x, t)
         
@@ -59,3 +75,20 @@ class SimpleConvNet:
         grads['b2'] = self.layers['Affine2'].db
         
         return grads
+    
+    def save_params(self, file_name="params.pkl"):
+        params = {}
+        for key, val in self.params.items():
+            params[key] = val
+        with open(file_name, 'wb') as f:
+            pickle.dump(params, f)
+
+    def load_params(self, file_name="params.pkl"):
+        with open(file_name, 'rb') as f:
+            params = pickle.load(f)
+        for key, val in params.items():
+            self.params[key] = val
+
+        for i, key in enumerate(['Conv1', 'Affine1', 'Affine2']):
+            self.layers[key].W = self.params['W' + str(i+1)]
+            self.layers[key].b = self.params['b' + str(i+1)]
